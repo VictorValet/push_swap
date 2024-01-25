7c51dab994de8ac76a707f27a3001321e0e9c48a
Victor Valet <vvalet@fu-r6-p5.s19.be>
2024-01-25 15:05:54 +0100

    changing from lists to tabs

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: vvalet <vvalet@student.s19.be >            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/27 13:32:00 by vvalet            #+#    #+#             */
-/*   Updated: 2024/01/25 14:54:07 by vvalet           ###   ########.fr       */
+/*   Updated: 2024/01/25 15:05:47 by vvalet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,31 @@
 
 int	main(int argc, char **argv)
 {
-	t_list	*stack_a;
-	t_list	*stack_b;
+	// t_list	*stack_a;
+	// t_list	*stack_b;
+	// int		*stack_a;
+	// int		*stack_b;
 
 	if (argc == 1)
 		return (0);
 	argc--;
 	list_validity(argc, argv);
-	stack_a = fill_stack(argc, argv);
-	stack_b = NULL;
-	if (argc <= 5)
-		sort_small(&stack_a, &stack_b);
-	else
-		sort_large(&stack_a, &stack_b);
-	ft_lstclear(&stack_a, del_content);
-	return (0);
+
+	int	*stack;
+
+	stack = ft_calloc(argc, sizeof(int));//protec
+	while (argc > 0)
+	{
+		stack[argc - 1] = ft_atoi(argv[argc]);
+		argc--;
+	}
+
+	// stack_a = fill_stack(argc, argv);
+	// stack_b = NULL;
+	// if (argc <= 5)
+	// 	sort_small(&stack_a, &stack_b);
+	// else
+	// 	sort_large(&stack_a, &stack_b);
+	// ft_lstclear(&stack_a, del_content);
+	// return (0);
 }
